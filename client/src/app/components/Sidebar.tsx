35588213f675ae2aa607a7ba661d5f3683db6a15
Remy-Tresor-250 <niyitegekatresor@gmail.com>
2023-05-11 22:25:05 +0200

    modifying sidebar

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -6,7 +6,11 @@ const Sidebar = () => {
   return (
     <div className={`${styles.flexStart} sm:hidden mdsm:items-start xl:w-[340px] p-2 fixed h-[100%]`}>
         <div className={`${styles.flexCenter} w-14 h-14 hover-animation`}>
-            {/* <Image src={}/> */}
+            <Image src="../logo2.svg" width={100} height={100} alt="logo"/>
+        </div>
+
+        <div className={``}>
+            
         </div>
     </div>
   )
