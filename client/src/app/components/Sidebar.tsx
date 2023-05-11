d5a36c0fa4614b5fcfed28b60e70999bd4e29764
Remy-Tresor-250 <niyitegekatresor@gmail.com>
2023-05-11 22:37:54 +0200

    done with the logo on the sidebar

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -1,19 +1,21 @@
-import React from 'react'
-import styles from '../styles'
-import Image from 'next/image'
+import React from "react";
+import styles from "../styles";
+import Image from "next/image";
 
 const Sidebar = () => {
   return (
-    <div className={`${styles.flexStart} sm:hidden mdsm:items-start xl:w-[340px] p-2 fixed h-[100%]`}>
-        <div className={`${styles.flexCenter} w-14 h-14 hover-animation`}>
-            <Image src="../logo2.svg" width={100} height={100} alt="logo"/>
-        </div>
+    <div
+      className={`${styles.flexStart} sm:hidden mdsm:items-start xl:w-[340px] p-2 fixed h-[100%]`}
+    >
+      <div
+        className={`${styles.flexCenter} hover-logo w-auto h-auto transition dutation-200 ease-out mdsm:px-0 w-[72px] h-[72px] cursor-pointer`}
+      >
+        <Image src="../logo2.svg" width={100} height={100} alt="logo" />
+      </div>
 
-        <div className={``}>
-            
-        </div>
+      <div className={``}></div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
