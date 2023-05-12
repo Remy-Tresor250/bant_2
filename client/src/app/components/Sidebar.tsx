5cbdb4b04b02bca743e639d7eb62d639f945235a
Remy-Tresor-250 <niyitegekatresor@gmail.com>
2023-05-12 06:07:31 +0200

    Remodified whole project and changd the name

--- client/src/app/components/Sidebar.tsx
+++ client/src/app/components/Sidebar.tsx
@@ -2,7 +2,16 @@ import React from "react";
 import styles from "../styles";
 import Image from "next/image";
 import SidebarLink from "./SidebarLink";
-import { faHome, faBell, faHashtag, faInbox, faBookmark, faClipboard, faUser, faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import {
+  faHome,
+  faBell,
+  faHashtag,
+  faInbox,
+  faBookmark,
+  faClipboard,
+  faUser,
+  faEllipsis,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
@@ -12,7 +21,7 @@ const Sidebar = () => {
       <div
         className={`${styles.flexCenter} hover-logo w-auto h-auto transition dutation-200 ease-out mdsm:px-0 w-[72px] h-[72px] cursor-pointer`}
       >
-        <Image src="../logo2.svg" width={60} height={60} alt="logo" />
+        <Image src="../logo1.svg" width={60} height={60} alt="logo" />
       </div>
 
       <div className={`gap-[1rem] flex flex-col mt-4 mb-2.5`}>
@@ -26,7 +35,11 @@ const Sidebar = () => {
         <SidebarLink text="More" Icon={faEllipsis} active={false} />
       </div>
 
-      <button className={`${styles.flexCenter} bg-moderateBlue rounded-[10rem] px-[10%] py-[1rem] font-bold text-[1.1em]`}></button>
+      <button
+        className={`${styles.flexCenter} bg-moderateBlue rounded-[10rem] px-[15%] py-[1rem] font-bold text-[1.1em]`}
+      >
+        Post
+      </button>
     </div>
   );
 };
